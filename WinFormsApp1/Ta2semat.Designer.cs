8c7b86e2fdd03e239f7abc187d34aa4271625803
kerols1234 <kerolssamer348@gmail.com>
2022-09-13 15:02:02 +0200

    random box

--- WinFormsApp1/Ta2semat.Designer.cs
+++ WinFormsApp1/Ta2semat.Designer.cs
@@ -45,62 +45,56 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(40, 136);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(35, 102);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(212, 364);
+            this.listBox1.Size = new System.Drawing.Size(186, 274);
             this.listBox1.TabIndex = 0;
             // 
             // txtraba
             // 
-            this.txtraba.Location = new System.Drawing.Point(41, 75);
-            this.txtraba.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtraba.Location = new System.Drawing.Point(36, 56);
             this.txtraba.Name = "txtraba";
             this.txtraba.PlaceholderText = "الاسم";
-            this.txtraba.Size = new System.Drawing.Size(185, 27);
+            this.txtraba.Size = new System.Drawing.Size(162, 23);
             this.txtraba.TabIndex = 4;
             this.txtraba.TextChanged += new System.EventHandler(this.txtraba_TextChanged);
             // 
             // listBox5
             // 
             this.listBox5.FormattingEnabled = true;
-            this.listBox5.ItemHeight = 20;
-            this.listBox5.Location = new System.Drawing.Point(716, 657);
-            this.listBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox5.ItemHeight = 15;
+            this.listBox5.Location = new System.Drawing.Point(626, 493);
             this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(185, 244);
+            this.listBox5.Size = new System.Drawing.Size(162, 184);
             this.listBox5.TabIndex = 12;
             this.listBox5.SelectedIndexChanged += new System.EventHandler(this.listBox5_SelectedIndexChanged);
             // 
             // listBox6
             // 
             this.listBox6.FormattingEnabled = true;
-            this.listBox6.ItemHeight = 20;
-            this.listBox6.Location = new System.Drawing.Point(488, 657);
-            this.listBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox6.ItemHeight = 15;
+            this.listBox6.Location = new System.Drawing.Point(427, 493);
             this.listBox6.Name = "listBox6";
-            this.listBox6.Size = new System.Drawing.Size(185, 244);
+            this.listBox6.Size = new System.Drawing.Size(162, 184);
             this.listBox6.TabIndex = 11;
             // 
             // listBox7
             // 
             this.listBox7.FormattingEnabled = true;
-            this.listBox7.ItemHeight = 20;
-            this.listBox7.Location = new System.Drawing.Point(266, 657);
-            this.listBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox7.ItemHeight = 15;
+            this.listBox7.Location = new System.Drawing.Point(233, 493);
             this.listBox7.Name = "listBox7";
-            this.listBox7.Size = new System.Drawing.Size(185, 244);
+            this.listBox7.Size = new System.Drawing.Size(162, 184);
             this.listBox7.TabIndex = 10;
             // 
             // listBox8
             // 
             this.listBox8.FormattingEnabled = true;
-            this.listBox8.ItemHeight = 20;
-            this.listBox8.Location = new System.Drawing.Point(41, 657);
-            this.listBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox8.ItemHeight = 15;
+            this.listBox8.Location = new System.Drawing.Point(36, 493);
             this.listBox8.Name = "listBox8";
-            this.listBox8.Size = new System.Drawing.Size(185, 244);
+            this.listBox8.Size = new System.Drawing.Size(162, 184);
             this.listBox8.TabIndex = 9;
             // 
             // ADD
@@ -113,10 +107,9 @@
             this.ADD.FillColor = System.Drawing.Color.DarkSlateBlue;
             this.ADD.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.ADD.ForeColor = System.Drawing.Color.White;
-            this.ADD.Location = new System.Drawing.Point(240, 580);
-            this.ADD.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ADD.Location = new System.Drawing.Point(210, 435);
             this.ADD.Name = "ADD";
-            this.ADD.Size = new System.Drawing.Size(192, 55);
+            this.ADD.Size = new System.Drawing.Size(168, 41);
             this.ADD.TabIndex = 13;
             this.ADD.Text = "نزود حد جديد";
             this.ADD.Click += new System.EventHandler(this.ADD_Click);
@@ -136,10 +129,9 @@
             "سنة تانية",
             "سنة تالتة",
             "سنة رابعة و خامسة"});
-            this.guna2ComboBox1.Location = new System.Drawing.Point(40, 13);
-            this.guna2ComboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.guna2ComboBox1.Location = new System.Drawing.Point(35, 10);
             this.guna2ComboBox1.Name = "guna2ComboBox1";
-            this.guna2ComboBox1.Size = new System.Drawing.Size(186, 36);
+            this.guna2ComboBox1.Size = new System.Drawing.Size(163, 36);
             this.guna2ComboBox1.TabIndex = 14;
             this.guna2ComboBox1.SelectedIndexChanged += new System.EventHandler(this.guna2ComboBox1_SelectedIndexChanged);
             // 
@@ -153,10 +145,9 @@
             this.btnpush.FillColor = System.Drawing.Color.SteelBlue;
             this.btnpush.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnpush.ForeColor = System.Drawing.Color.White;
-            this.btnpush.Location = new System.Drawing.Point(23, 580);
-            this.btnpush.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnpush.Location = new System.Drawing.Point(20, 435);
             this.btnpush.Name = "btnpush";
-            this.btnpush.Size = new System.Drawing.Size(192, 55);
+            this.btnpush.Size = new System.Drawing.Size(168, 41);
             this.btnpush.TabIndex = 15;
             this.btnpush.Text = "Push";
             this.btnpush.Click += new System.EventHandler(this.btnpush_Click);
@@ -171,27 +162,28 @@
             this.btnpuzzle.FillColor = System.Drawing.Color.Crimson;
             this.btnpuzzle.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnpuzzle.ForeColor = System.Drawing.Color.White;
-            this.btnpuzzle.Location = new System.Drawing.Point(507, 580);
-            this.btnpuzzle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnpuzzle.Location = new System.Drawing.Point(444, 435);
             this.btnpuzzle.Name = "btnpuzzle";
-            this.btnpuzzle.Size = new System.Drawing.Size(192, 55);
+            this.btnpuzzle.Size = new System.Drawing.Size(168, 41);
             this.btnpuzzle.TabIndex = 16;
             this.btnpuzzle.Text = "Puzzle";
+            this.btnpuzzle.Click += new System.EventHandler(this.btnpuzzle_Click);
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(292, 16);
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(256, 12);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(646, 484);
+            this.listBox2.Size = new System.Drawing.Size(566, 364);
             this.listBox2.TabIndex = 17;
             // 
             // Ta2semat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 959);
+            this.ClientSize = new System.Drawing.Size(850, 562);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.btnpuzzle);
             this.Controls.Add(this.btnpush);
@@ -204,7 +196,6 @@
             this.Controls.Add(this.txtraba);
             this.Controls.Add(this.listBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Ta2semat";
             this.Text = "Ta2semat";
             this.ResumeLayout(false);
--- WinFormsApp1/Ta2semat.cs
+++ WinFormsApp1/Ta2semat.cs
@@ -1,22 +1,14 @@
 ﻿using MYF_APP;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
     public partial class Ta2semat : Form
     {
-        functions fn = new functions();
-        String query;
+        readonly functions fn = new functions();
+        string query;
         public Ta2semat()
         {
             InitializeComponent();
@@ -24,14 +16,14 @@ namespace WinFormsApp1
 
         private void ADD_Click(object sender, EventArgs e)
         {
-             Add_new_one add_New_One = new Add_new_one();
+            Add_new_one add_New_One = new Add_new_one();
             add_New_One.Show();
             this.Close();
         }
-       
+
         private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void guna2Button1_Click(object sender, EventArgs e)
@@ -39,22 +31,22 @@ namespace WinFormsApp1
             //query = "Select Name from myfdb Where Age = 'سنة اولى' and Name like'" + txtola.Text + "%'";
             DataSet ds = fn.GetData(query);
             int i;
-            
+
             for (i = 0; i < ds.Tables[0].Rows.Count; i++)
             {
                 listBox1.Items.Add(ds.Tables[0].Rows[i][0].ToString());
             }
-            
+
         }
 
         private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            String category = guna2ComboBox1.Text;
+            string category = guna2ComboBox1.Text;
             query = "Select Name from myf Where Age = N'" + category + "'";
             showItemList(query);
         }
-        private void showItemList(String query)
+        private void showItemList(string query)
         {
             DataSet ds = fn.GetData(query);
             int i;
@@ -67,7 +59,7 @@ namespace WinFormsApp1
         private void txtraba_TextChanged(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            String category = guna2ComboBox1.Text;
+            string category = guna2ComboBox1.Text;
             query = "Select Name from myf Where Age = N'" + category + "' and Name like N'" + txtraba.Text + "%'";
 
             using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["database"].ConnectionString))
@@ -99,6 +91,47 @@ namespace WinFormsApp1
         private void btnpush_Click(object sender, EventArgs e)
         {
             listBox2.Items.Add(listBox1.SelectedItem);
+        }
+
+        private void btnpuzzle_Click(object sender, EventArgs e)
+        {
+            listBox5.Items.Clear();
+            listBox6.Items.Clear();
+            listBox7.Items.Clear();
+            listBox8.Items.Clear();
+
+            var data = listBox2.Items.Cast<string>().ToArray();
+            var size = data.Length;
+
+            int boxSize = size / 4;
+
+            int fir = boxSize;
+            int sec = boxSize;
+            int thi = boxSize;
+
+            int remander = size % 4;
+
+            if (remander > 0)
+            {
+                fir += 1;
+                if (remander > 1)
+                {
+                    sec += 1;
+                    if (remander > 2)
+                    {
+                        thi += 1;
+                    }
+                }
+            }
+
+            var r = new Random();
+
+            data = data.OrderBy(i => r.Next()).ToArray();
+
+            listBox5.Items.AddRange(data.Take(fir).ToArray());
+            listBox6.Items.AddRange(data.Skip(fir).Take(sec).ToArray());
+            listBox7.Items.AddRange(data.Skip(fir + sec).Take(thi).ToArray());
+            listBox8.Items.AddRange(data.Skip(fir + sec + thi).Take(boxSize).ToArray());
         }
     }
 }
