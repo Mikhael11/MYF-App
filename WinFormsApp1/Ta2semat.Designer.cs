b83871f9217db80d82a6bba6e59b203fe43525e8
Mikhael El Kess Youhanna <mikhael1112000@gmail.com>
2022-09-13 12:53:11 +0200

    Add buttons

--- WinFormsApp1/Ta2semat.Designer.cs
+++ WinFormsApp1/Ta2semat.Designer.cs
@@ -43,24 +43,28 @@
             this.listBox8 = new System.Windows.Forms.ListBox();
             this.ADD = new Guna.UI2.WinForms.Guna2Button();
             this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.btnpush = new Guna.UI2.WinForms.Guna2Button();
+            this.btnpuzzle = new Guna.UI2.WinForms.Guna2Button();
             ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(36, 141);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(40, 136);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(186, 274);
+            this.listBox1.Size = new System.Drawing.Size(212, 364);
             this.listBox1.TabIndex = 0;
             // 
             // txtraba
             // 
-            this.txtraba.Location = new System.Drawing.Point(36, 95);
+            this.txtraba.Location = new System.Drawing.Point(41, 75);
+            this.txtraba.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtraba.Name = "txtraba";
-            this.txtraba.PlaceholderText = "سنة رابعة و خامسة";
-            this.txtraba.Size = new System.Drawing.Size(162, 23);
+            this.txtraba.PlaceholderText = "الاسم";
+            this.txtraba.Size = new System.Drawing.Size(185, 27);
             this.txtraba.TabIndex = 4;
             this.txtraba.TextChanged += new System.EventHandler(this.txtraba_TextChanged);
             // 
@@ -90,11 +94,13 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.guna2DataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
             this.guna2DataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.guna2DataGridView1.Location = new System.Drawing.Point(411, 12);
+            this.guna2DataGridView1.Location = new System.Drawing.Point(354, 13);
+            this.guna2DataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.guna2DataGridView1.Name = "guna2DataGridView1";
             this.guna2DataGridView1.RowHeadersVisible = false;
+            this.guna2DataGridView1.RowHeadersWidth = 51;
             this.guna2DataGridView1.RowTemplate.Height = 25;
-            this.guna2DataGridView1.Size = new System.Drawing.Size(479, 398);
+            this.guna2DataGridView1.Size = new System.Drawing.Size(547, 531);
             this.guna2DataGridView1.TabIndex = 8;
             this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
             this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
@@ -121,47 +127,53 @@
             // الاسم
             // 
             this.الاسم.HeaderText = "السنة";
+            this.الاسم.MinimumWidth = 6;
             this.الاسم.Name = "الاسم";
             // 
             // السنة
             // 
             this.السنة.HeaderText = "الاسم";
+            this.السنة.MinimumWidth = 6;
             this.السنة.Name = "السنة";
             // 
             // listBox5
             // 
             this.listBox5.FormattingEnabled = true;
-            this.listBox5.ItemHeight = 15;
-            this.listBox5.Location = new System.Drawing.Point(1344, 531);
+            this.listBox5.ItemHeight = 20;
+            this.listBox5.Location = new System.Drawing.Point(731, 708);
+            this.listBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(162, 259);
+            this.listBox5.Size = new System.Drawing.Size(185, 344);
             this.listBox5.TabIndex = 12;
             // 
             // listBox6
             // 
             this.listBox6.FormattingEnabled = true;
-            this.listBox6.ItemHeight = 15;
-            this.listBox6.Location = new System.Drawing.Point(845, 531);
+            this.listBox6.ItemHeight = 20;
+            this.listBox6.Location = new System.Drawing.Point(476, 708);
+            this.listBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox6.Name = "listBox6";
-            this.listBox6.Size = new System.Drawing.Size(162, 259);
+            this.listBox6.Size = new System.Drawing.Size(185, 344);
             this.listBox6.TabIndex = 11;
             // 
             // listBox7
             // 
             this.listBox7.FormattingEnabled = true;
-            this.listBox7.ItemHeight = 15;
-            this.listBox7.Location = new System.Drawing.Point(423, 531);
+            this.listBox7.ItemHeight = 20;
+            this.listBox7.Location = new System.Drawing.Point(247, 708);
+            this.listBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox7.Name = "listBox7";
-            this.listBox7.Size = new System.Drawing.Size(162, 259);
+            this.listBox7.Size = new System.Drawing.Size(185, 344);
             this.listBox7.TabIndex = 10;
             // 
             // listBox8
             // 
             this.listBox8.FormattingEnabled = true;
-            this.listBox8.ItemHeight = 15;
-            this.listBox8.Location = new System.Drawing.Point(36, 531);
+            this.listBox8.ItemHeight = 20;
+            this.listBox8.Location = new System.Drawing.Point(41, 708);
+            this.listBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox8.Name = "listBox8";
-            this.listBox8.Size = new System.Drawing.Size(162, 259);
+            this.listBox8.Size = new System.Drawing.Size(185, 344);
             this.listBox8.TabIndex = 9;
             // 
             // ADD
@@ -174,9 +186,10 @@
             this.ADD.FillColor = System.Drawing.Color.DarkSlateBlue;
             this.ADD.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.ADD.ForeColor = System.Drawing.Color.White;
-            this.ADD.Location = new System.Drawing.Point(210, 435);
+            this.ADD.Location = new System.Drawing.Point(240, 580);
+            this.ADD.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ADD.Name = "ADD";
-            this.ADD.Size = new System.Drawing.Size(168, 41);
+            this.ADD.Size = new System.Drawing.Size(192, 55);
             this.ADD.TabIndex = 13;
             this.ADD.Text = "نزود حد جديد";
             this.ADD.Click += new System.EventHandler(this.ADD_Click);
@@ -196,17 +209,54 @@
             "سنة تانية",
             "سنة تالتة",
             "سنة رابعة و خامسة"});
-            this.guna2ComboBox1.Location = new System.Drawing.Point(36, 39);
+            this.guna2ComboBox1.Location = new System.Drawing.Point(40, 13);
+            this.guna2ComboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.guna2ComboBox1.Name = "guna2ComboBox1";
-            this.guna2ComboBox1.Size = new System.Drawing.Size(163, 36);
+            this.guna2ComboBox1.Size = new System.Drawing.Size(186, 36);
             this.guna2ComboBox1.TabIndex = 14;
             this.guna2ComboBox1.SelectedIndexChanged += new System.EventHandler(this.guna2ComboBox1_SelectedIndexChanged);
             // 
+            // btnpush
+            // 
+            this.btnpush.BorderRadius = 26;
+            this.btnpush.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnpush.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnpush.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnpush.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnpush.FillColor = System.Drawing.Color.SteelBlue;
+            this.btnpush.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnpush.ForeColor = System.Drawing.Color.White;
+            this.btnpush.Location = new System.Drawing.Point(23, 580);
+            this.btnpush.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnpush.Name = "btnpush";
+            this.btnpush.Size = new System.Drawing.Size(192, 55);
+            this.btnpush.TabIndex = 15;
+            this.btnpush.Text = "Push";
+            // 
+            // btnpuzzle
+            // 
+            this.btnpuzzle.BorderRadius = 26;
+            this.btnpuzzle.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnpuzzle.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnpuzzle.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnpuzzle.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnpuzzle.FillColor = System.Drawing.Color.Crimson;
+            this.btnpuzzle.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnpuzzle.ForeColor = System.Drawing.Color.White;
+            this.btnpuzzle.Location = new System.Drawing.Point(507, 580);
+            this.btnpuzzle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnpuzzle.Name = "btnpuzzle";
+            this.btnpuzzle.Size = new System.Drawing.Size(192, 55);
+            this.btnpuzzle.TabIndex = 16;
+            this.btnpuzzle.Text = "Puzzle";
+            // 
             // Ta2semat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1540, 812);
+            this.ClientSize = new System.Drawing.Size(972, 1055);
+            this.Controls.Add(this.btnpuzzle);
+            this.Controls.Add(this.btnpush);
             this.Controls.Add(this.guna2ComboBox1);
             this.Controls.Add(this.ADD);
             this.Controls.Add(this.listBox5);
@@ -217,6 +267,7 @@
             this.Controls.Add(this.txtraba);
             this.Controls.Add(this.listBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Ta2semat";
             this.Text = "Ta2semat";
             ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).EndInit();
@@ -238,5 +289,7 @@
         private ListBox listBox8;
         private Guna.UI2.WinForms.Guna2Button ADD;
         private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private Guna.UI2.WinForms.Guna2Button btnpush;
+        private Guna.UI2.WinForms.Guna2Button btnpuzzle;
     }
 }
